57010c9386009d4911b6ea217b48e15d0107f73d
Andy Wang <AndrewWang996@gmail.com>
2015-09-20 03:18:01 -0400

    scraping series first commit, need inteface between this and bash script

--- main.sh
+++ main.sh
@@ -2,4 +2,6 @@ mkdir Images
 python scraper.py
 cd Images
 convert *.png chapter.pdf
+rm *.png
+cd ../
 
--- scraper.py
+++ scraper.py
@@ -1,40 +1,68 @@
 
 import urllib, urllib2, re
-
-opener = urllib2.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-
-## example link:
-## http://2.bp.blogspot.com/-ANiS2T94Sdg/VfF1NJY6kEI/AAAAAAAC2qw/nVbQGfhTVbI/s16000-Ic42/000.png
-
-# source = urllib.urlopen('http://kissmanga.com/Manga/Toriko/Ch-338--Joa-VS-Midora--?id=241108').read()
-source = opener.open('http://kissmanga.com/Manga/Toriko/Ch-338--Joa-VS-Midora--?id=241108').read()
-
-# print(source)
-
-## every image name is an abbreviation composed by capital letters, so...
-for link in re.findall('http://.*[0-9][0-9][0-9].png', source):
-    cleanLink = link
-
-
-    ## the code above just prints the link;
-    ## if you want to actually download, set the flag below to True
-
-    actually_download = True
-    if actually_download:
-        filename = cleanLink.split('/')[-1]
-        urllib.urlretrieve(cleanLink, 'Images/' + filename)
-
-'''
 from bs4 import BeautifulSoup
-import requests
-r  = requests.get('http://kissmanga.com/Manga/Toriko/Ch-338--Joa-VS-Midora--?id=241108')
 
-data = r.text
+def getSeriesTitle(chapterOrSeriesURL):
+	'''
+	gets title of chapter from KissManga chapter URL.
+	Example: http://kissmanga.com/Manga/Uzumaki/Uzumaki-Chapter-20?id=56954
+	'''
+	domainURL = 'kissmanga.com/Manga/'
+	remainingString = chapterOrSeriesURL.split(domainURL)[-1]
+	seriesTitle = remainingString.split('/')[0]
+	return seriesTitle
 
-print(data)
 
-soup = BeautifulSoup(data)
 
-print(soup)
-'''
+def scrapeChapter(chapterURL):
+	'''
+	scrapes an entire chapter from the given URL of the KissManga domain
+	downloads as multiple png's
+	'''
+	opener = urllib2.build_opener()
+	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+
+
+	source = opener.open(chapterURL).read()
+
+
+	## example link:
+	## http://2.bp.blogspot.com/-ANiS2T94Sdg/VfF1NJY6kEI/AAAAAAAC2qw/nVbQGfhTVbI/s16000-Ic42/000.png
+	for link in re.findall('http://.*[0-9][0-9][0-9].png', source):
+	    cleanLink = link
+
+
+	    ## the code above just prints the link;
+	    ## if you want to actually download, set the flag below to True
+
+	    actually_download = True
+	    if actually_download:
+	        filename = cleanLink.split('/')[-1]
+	        urllib.urlretrieve(cleanLink, 'Images/' + filename)
+
+def scrapeSeries(seriesURL):
+	'''
+	<a href="/Manga/Uzumaki/Uzumaki-Chapter-2?id=56786" title="Read Uzumaki Uzumaki Chapter 002 online">Uzumaki Chapter 002</a>
+	'''
+	opener = urllib2.build_opener()
+	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+	source = opener.open(seriesURL).read()
+
+	seriesTitle = getSeriesTitle(seriesURL)
+
+	prefixString = '<a href="/Manga/' + seriesTitle
+ 	htmlLines = [line for line in source.split("\n") if line.startswith(prefixString)]
+ 	chapterRelativeURLs = [line.split('\"')[1] for line in htmlLines]
+
+ 	chapterPrefixString = 'http://kissmanga.com'
+ 	chapterURLs = [chapterPrefixString + line for line in chapterRelativeURLs]
+
+ 	for chapterURL in chapterURLs:
+ 		scrapeChapter(chapterURL)
+	# TODO finish this method
+
+scrapeSeries('http://kissmanga.com/Manga/Uzumaki')
+# scrapeChapter('http://kissmanga.com/Manga/Toriko/Ch-338--Joa-VS-Midora--?id=241108')
+
+
+
